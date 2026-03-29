d1017e7b428d4387d1eefe9bd654018475254078
Derek Dao <ducdao11092002@gmail.com>
2026-03-29 23:58:22 +0700

    Neetcode: Easy: Invert Binary Tree Problem

---
+++ InvertBinaryTree/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        var tree = BuildTree(new int[] {4,2,7,1,3,6,9});
+
+        var result = solution.InvertTree(tree);
+        Console.WriteLine("Hello, World!");
+    }
+
+    private static TreeNode BuildTree(int[] nodes)
+    {
+        TreeNode root = new TreeNode(nodes[0]);
+
+        Queue<TreeNode> queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+        int i = 1;
+        while (i < nodes.Length && queue.Count > 0)
+        {
+            // Lấy Node cha ra để tìm con cho nó
+            TreeNode parent = queue.Dequeue();
+
+            // Tìm con bên trái
+            if (i < nodes.Length && nodes[i] != null)
+            {
+                parent.left = new TreeNode(nodes[i]);
+                queue.Enqueue(parent.left);
+            }
+            i++;
+
+            // Tìm con bên phải
+            if (i < nodes.Length && nodes[i] != null)
+            {
+                parent.right = new TreeNode(nodes[i]);
+                queue.Enqueue(parent.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+}
---
+++ InvertBinaryTree/Solution.cs
@@ -0,0 +1,15 @@
+class Solution
+{
+    public Solution() { }
+
+    public TreeNode InvertTree(TreeNode root)
+    {
+        if (root == null) return null;
+
+        TreeNode temp = root.left;
+        root.left = InvertTree(root.right);
+        root.right = InvertTree(temp);
+
+        return root;
+    }
+}
