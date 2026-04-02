13d5b19ccfb5398256e4b233637faf3f69fa75f1
Derek Dao <ducdao11092002@gmail.com>
2026-04-02 23:22:02 +0700

    Neetcode: Easy: Linked List Cycle Problem

---
+++ LinkedListCycle/Program.cs
@@ -0,0 +1,53 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        var node = CreateLinkedListCycle(new int[] { 1,2 }, -1);
+        // var node = CreateLinkedList(new int[] { 3, 2, 0, -4 });
+        var result = solution.HasCycle(node);
+        Console.WriteLine("Hello, World!");
+    }
+
+    private static ListNode CreateLinkedListCycle(int[] nums, int pos)
+    {
+        if (nums == null || nums.Length == 0) return null;
+
+        ListNode head = new ListNode(nums[0]);
+        ListNode current = head;
+        ListNode cycleTargetNode = null;
+
+        if (pos == 0) cycleTargetNode = head;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            current.next = new ListNode(nums[i]);
+            current = current.next;
+
+            if (i == pos)
+            {
+                cycleTargetNode = current;
+            }
+        }
+
+        if (pos != -1 && cycleTargetNode != null)
+        {
+            current.next = cycleTargetNode;
+        }
+
+        return head;
+    }
+
+    private static ListNode CreateLinkedList(int[] nums)
+    {
+        var node = new ListNode(nums[0]);
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            node.next = new ListNode(nums[i]);
+
+        }
+
+        return node;
+    }
+}
---
+++ LinkedListCycle/Solution.cs
@@ -0,0 +1,21 @@
+class Solution
+{
+    public Solution() { }
+
+    public bool HasCycle(ListNode head)
+    {
+        if (head == null) return false;
+
+        ListNode p1 = head, p2 = head;
+
+        while (p2 != null && p2.next != null)
+        {
+            p1 = p1.next;
+            p2 = p2.next.next;
+
+            if (p1 == p2) return true;
+        }
+
+        return false;
+    }
+}
