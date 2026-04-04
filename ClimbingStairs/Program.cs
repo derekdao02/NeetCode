81c96689aa7fd35df83934744a03cb287c12dfdc
Derek Dao <ducdao11092002@gmail.com>
2026-04-04 23:29:55 +0700

    Neetcode: Easy: Climbing Stairs Problem

---
+++ ClimbingStairs/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        var result = solution.ClimbStairs(4);
+        Console.WriteLine(result);
+    }
+}
